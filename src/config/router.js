9c1c1607ed039597944ec844ec508a3b026d03ce
Evgeny Shmakov <sjs-master@yandex.ru>
2017-07-07 22:16:04 +0700

    delete files config rout

--- src/config/router.js
+++ src/config/router.js
@@ -1,35 +1,10 @@
 import React from 'react';
 import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
 
-import FeedUsers from '../containers/FeedUsersContainer';
 import UserDetail from '../containers/UserDetailContainer';
 import Home from '../containers/HomeContainer';
-import CameraRollPicker from '../containers/CameraRollPickerContainer';
-import CameraPicker from '../containers/CameraContainer';
 
 import I18n from '../i18n/index'
-
-export const FeedStack = StackNavigator({
-  Feed: {
-    screen: FeedUsers,
-    navigationOptions: {
-      title: I18n.t('LABOR'),
-      headerBackTitle: null,
-    },
-  },
-  Details: {
-    screen: UserDetail,
-    navigationOptions: ({navigation, screenProps}) => ({
-        cardStack: {
-            gesturesEnabled: true,
-        },
-        title: 'splash store',
-    }),
-  },
-}, {
-    mode: 'modal',
-    headerMode: 'none',
-});
 
 export const Root = StackNavigator({
     Home: {
@@ -39,22 +14,15 @@ export const Root = StackNavigator({
             headerBackTitle: null,
         },
     },
-    CameraRoll: {
-        screen: CameraRollPicker,
-        navigationOptions: {
-            title: I18n.t('TAKE_PHOTO'),
-        },
-    },
-    Camera: {
-        screen: CameraPicker,
-        navigationOptions: {
-            title: I18n.t('TAKE_PHOTO'),
-        },
-    },
-    Feed: {
-        screen: FeedStack,
-    },
-
+    Details: {
+        screen: UserDetail,
+        navigationOptions: ({navigation, screenProps}) => ({
+            cardStack: {
+                gesturesEnabled: true,
+            },
+            title: 'splash store',
+        }),
+    }
 }, {
   //mode: 'card',
   //headerMode: 'none',
